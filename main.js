7ad0f3a4e64d0f91887db31046667832851f034f
Marcela Pino <marcelap99@aol.com>
2017-10-27 17:59:17 -0400

    submitting after corrections

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
-//this is nav stuff
+// this is nav stuff //
 
 $.stellar();
 $('nav').onePageNav();
 
 //When user clicks menu toggle the nav ul //
-$('#menu').click(function(){
+$('#menu').click(function() {
   $('nav ul').slideToggle();
 });
 
 const nav = $('nav ul');
-$(window).resize(function(){
+$(window).resize(function() {
   const width = $(window).width();
-  if(width > 768 && nav.is (':hidden')){
+  if(width > 768 && nav.is (':hidden')) {
     nav.removeAttr('style');
   }
 });
 
-$('section').mouseenter(function(){
+$('section').mouseenter(function() {
   $(this).find('.thumbnail').fadeIn();
-})
+});
 
-$('section').mouseleave(function(){
+$('section').mouseleave(function() {
   $(this).find('.thumbnail').fadeOut();
-})
+});
